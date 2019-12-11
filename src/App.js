65df3b5f065eb50bf6768d409d8e544b846944e3
Hong Shik Branden Kim <kimploo@gmail.com>
2019-12-11 11:08:22 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+export default class App extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  handleClick = () => {
+    console.log(this);
+  }
+
+  render(){
+    return(
+      <div className="App">
       <header className="App-header">
         practice this binding
       </header>
-      
+      <button type="button" onClick={this.handleClick}>
+        Click Me
+      </button>
     </div>
-  );
-}
+    )
+  }
 
-export default App;
+}
